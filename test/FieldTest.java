8e436cdd866f400bcb67761bc625d6ad04d2c654
JulyMak <makarova.y672@gmail.com>
2017-03-06 09:26:53 +0300

    field

--- test/FieldTest.java
+++ test/FieldTest.java
@@ -7,22 +7,40 @@ public class FieldTest {
     public void addZero() {
         Field field = new Field(3);
         field.addZero(2,0);
+        field.addZero(1,1);
+        field.addZero(2,1);
         assertEquals('0', field.getElement(2,0));
+        assertEquals('0', field.getElement(1,1));
+        assertEquals('0', field.getElement(2,1));
+
     }
 
     @Test
     public void addCross() {
         Field field = new Field(3);
         field.addCross(2,0);
+        field.addCross(0,1);
+        field.addCross(1,2);
         assertEquals('X', field.getElement(2,0));
+        assertEquals('X', field.getElement(0,1));
+        assertEquals('X', field.getElement(1,2));
     }
 
     @Test
     public void clean() {
         Field field = new Field(3);
         field.addZero(2,0);
+        field.addCross(0,1);
+        field.addZero(2,1);
+        field.addCross(1,2);
         field.clean(2,0);
+        field.clean(0,1);
+        field.clean(2,1);
+        field.clean(1,2);
         assertEquals(0, field.getElement(2,0));
+        assertEquals(0, field.getElement(0,1));
+        assertEquals(0, field.getElement(2,1));
+        assertEquals(0, field.getElement(1,2));
     }
 
     @Test
